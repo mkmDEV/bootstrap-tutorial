57903d0fb5a18bd0f4ed13c5cdc0c500364271d5
mkmdev <mkmdev18@gmail.com>
2019-08-28 19:41:34 +0200

    + top-nav-bar, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
