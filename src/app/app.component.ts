ea686eb0c19273b25aca66dba1ed99a970e211a0
mkmdev <mkmdev18@gmail.com>
2019-08-28 20:19:57 +0200

    updated viewport meta tag and added author, description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title, Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bootstrap-tutorial';
+
+
+  constructor(
+    private titleService: Title,
+    private meta: Meta
+  ) {
+    titleService.setTitle('Blog Post - Start Bootstrap Template');
+    meta.updateTag({name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'});
+    meta.addTag({name: 'description', content: 'My first Angular8 project with Bootstrap4'}, true);
+    meta.addTag({name: 'author', content: 'MARSI, Kristóf Mátyás'}, true);
+  }
 }
